686793f97be1079d742812406a67c483cb999e6e
Daniel K <daniel.kendall.ed@gmail.com>
2024-12-07 17:18:11 -0700

    Separating effect data into CSV
    Having effect data in CSV will make it easier to add/edit/remove effects and keep all the relevant information for effects in a unified location.

--- src/game.c
+++ src/game.c
@@ -1,37 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Buffs/debuffs affecting other actions will be applied to die faces instead of characters.
-// On second thought, since die faces will be able to have multiple effects, applying buffs/debuffs to characters might make more sense.
-// Might need to limit buffs to fixed effects so they don't become immediately busted though.
-const char effects[][12] = {
-    // basic actions
-    "attack", // damages one target
-    "defend", // gives block, preventing incoming damage
-    "hide", // provides dodge/crit chance. either activating halves the value
-    "heal", // restores hp
-    "dud", // does nothing
-    // advanced actions
-    "purge", // removes all? of a specified effect from the target, if followed by another effect add the purged quantity to that effect's value
-    "cleanse", // removes value in buffs/debuffs/status effects from a target, could randomly select things to remove or have a preference list defined and available to the player
-    // face buffs (TODO: random or do you choose the face?)
-    "strengthen", // attacking
-    "fortify", // defending buff
-    "evade", // hiding buff
-    "vigor", // healing buff
-    // face debuffs (TODO: random or do you choose the face?)
-    "weaken", // attacking debuff
-    "erode", // defending debuff
-    "mark", // hiding debuff
-    "ennervate", // healing debuff
-    // status effects, decrease each turn and/or on activation
-    "burn", // damage over time, stacks-1 each turn
-    "freeze", // actions with lower value do not happen, remove stacks equal to the value of prevented actions
-    "poison", // if poison >= current HP, target dies
-    "wound", // increase damage to HP when target is hit
-    "curse", // increase status effect applications (burn, freeze, poison, wound)
-    "spiked", // deals damage to attackers, can't deal more damage than the attack?
-    };
+struct EffectData {
+    const char name[12];
+    const int ratio; // Divide value by ratio to get the end result. So an effect of value 10 with ratio 3 would have a final result of 3.
+    const char description[128]; // 128 should be enough space, but we'll see
+};
 
 // Whether added dice are treated as additional characters or not, they will be themed around some sort of archetype
 const char dieTypes[][10] = {
@@ -42,14 +16,17 @@ const char dieTypes[][10] = {
     "vampire", // 2 attack/heal, 1 strengthen, 1 vigor, 2 dud
     "pyromancer", // 2 burn attacks, 2 burn defends, 2 burns (base value on hybrid actions lower)
     "cryomancer", // 3 freeze attacks, 3 freeze (relies on freezing enemies to protect themselves)
+    "warlock", // 2 regen enemy, 2 channel regen, 1 poison enemy, 1 regen self. Give the enemy regen, convert it into regen for yourself or poison on the enemy.
     };
 
 // Effects are applied in the order applied to the die face,
 // so a vigor face upgraded with healing could benefit from the increased healing immediately,
 // while a healing face upgraded with vigor, would perform the heal before gaining the bonus
+// TODO: consider removing flat bonus due to the addition of ratios
 const char upgrades[][16] = {
     "plus", // adds a fixed value to an effect, costs more depending on the effect
     "repeat", // halves an effect's value (minimum 1) but causes it to apply an extra time
+    "sweep", // applies the effect to all valid targets, more interesting on positive die faces if each die is a separate character as in Slice & Dice
     "flat bonus", // gives a bonus effect that does not scale with face value
     "scaling bonus", // gives a bonus effect that scales with face value
     "result bonus", // gives a bonus effect that scales with the effectiveness of the base action
@@ -69,13 +46,25 @@ struct Die {
 };
 
 // TODO: use pointers to allow the dice list to have dynamic length
+// Most entities will start with 0 of all buffs, debuffs, and status effects, but some do not
 struct Entity {
+    int is_player; // 0: enemy, 1: player
+    int health;
+    int buffs[5], debuffs[4], statuses[4];
     struct Die dice[3];
 };
 
 
 int main(int argc, char **argv) {
-    printf("Testing");
+    FILE* reader = fopen("effects.csv","r");
+
+    if(!reader){
+        printf("Oh no");
+        return 1;
+    }
+    else {
+        // stuff
+    }    
 
     return 0;
 };
